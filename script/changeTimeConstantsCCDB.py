33110a033d0d49f7af10923e086520f9f3022491
izzy <izzy@jlab.org>
2024-07-15 15:33:25 -0400

    Update changeTimeConstantsCCDB.py
    Script now allows you to update timing using sector by sector info

--- script/changeTimeConstantsCCDB.py
+++ script/changeTimeConstantsCCDB.py
@@ -2,32 +2,19 @@ import pandas as pd
 import sys
 
 # Check if enough arguments are provided
-if len(sys.argv) < 4:
-    print("Usage: python3 script.py runNum fileCCDB timeShift")
+if len(sys.argv) < 9:
+    print("Usage: python3 changeTimeConstantsCCDB.py runNum fileCCDB sec1_shift sec2_shift sec3_shift sec4_shift sec5_shift sec6_shift")
     sys.exit(1)
 
 runNum = int(sys.argv[1])
 fileCCDB = sys.argv[2]
-timeShift = float(sys.argv[3])  # Accept timeShift as a command-line argument
+sector_shifts = [float(sys.argv[i]) for i in range(3, 9)]
 
-print(sys.argv[1])
-print(sys.argv[2])
-print(sys.argv[3])
+print(f"Run Number: {runNum}")
+print(f"CCDB File: {fileCCDB}")
+print(f"Time Shifts: {sector_shifts}")
 
 def readDatFile(filePath, ccdbBool, paramName):
-    """
-    function to read in a dat file and return a filled dataframe 
-
-    Parameters
-    ----------
-    filePath: STRING, dat file
-    ccdbBool: BOOLEAN, is dat file from ccdb or not (if TRUE, skip header)
-    paramName: STRING, gain or time
-    
-    Returns
-    ----------
-    df_out: filled dataframe
-    """
     channels = 48
     count = 0
 
@@ -37,43 +24,45 @@ def readDatFile(filePath, ccdbBool, paramName):
     parameter = []
 
     with open(filePath) as f:
-        if (ccdbBool == True):
-            next(f) #skip header
+        if ccdbBool:
+            next(f)  # Skip header
         for i, line in enumerate(f):
-            percentage = 100*(count/channels)
+            percentage = 100 * (count / channels)
             boxes_blk = ["\u2588"] * count
-            boxes_yt = [" "] * (channels-count)
-            if (percentage%5 == 0):
-                if (count == 0):
+            boxes_yt = [" "] * (channels - count)
+            if percentage % 5 == 0:
+                if count == 0:
                     print("Read %d channels\t\t[%s%s]" % (count, "".join(boxes_blk), "".join(boxes_yt)))
                 else:
                     print("Read %d channels\t[%s%s]" % (count, "".join(boxes_blk), "".join(boxes_yt)))
-            # print(line)
+
             splitd = line.split()
-            # print(splitd)
             sector.append(int(splitd[0]))
             halfsector.append(int(splitd[1]))
             ring.append(int(splitd[2]))
             parameter.append(float(splitd[3]))
 
-            count = count + 1
+            count += 1
         print("Read %d channels\t[%s%s]" % (count, "".join(boxes_blk), "".join(boxes_yt)))
         
         dict_info = {
-        "S": sector, 
-        "HS": halfsector, 
-        "R": ring, 
-        paramName: parameter
+            "S": sector, 
+            "HS": halfsector, 
+            "R": ring, 
+            paramName: parameter
         }
         
         df_info = pd.DataFrame(dict_info)
         df_info = df_info.sort_values(["R", "S"])
-        df_info.reset_index(inplace = True, drop = True)
+        df_info.reset_index(inplace=True, drop=True)
         
         return df_info
 
 df_ccdb = readDatFile(fileCCDB, True, "time")
+print("Head of original CCDB file:")
 print(df_ccdb.head())
+print("Tail of original CCDB file:")
+print(df_ccdb.tail())
 
 colNames = ["S", "HS", "R", "time"]
 df_compare = pd.DataFrame(columns=colNames)
@@ -81,12 +70,19 @@ df_compare["S"] = df_ccdb["S"]
 df_compare["HS"] = df_ccdb["HS"]
 df_compare["R"] = df_ccdb["R"]
 
-#-----TIME SHIFT PARAMETER-----#
-#see example for how these were calculated over at this entry https://logbooks.jlab.org/entry/4049486
-#timeShift = 0.2
-#timeShift = -5.921569
-#timeShift = -5.905573636363638
-df_compare["time"] = df_ccdb["time"] + timeShift
+# Ensure the 'S' column is integer type
+df_compare["S"] = df_compare["S"].astype(int)
+df_compare["HS"] = df_compare["HS"].astype(int)
+df_compare["R"] = df_compare["R"].astype(int)
 
+# Apply the time shifts to the time constants
+df_compare["time"] = df_ccdb.apply(lambda row: row["time"] + sector_shifts[int(row["S"]) - 1], axis=1)
+
+print("Head of new CCDB file:")
+print(df_compare.head())
+print("Tail of new CCDB file:")
 print(df_compare.tail())
-df_compare.to_csv("run%d_%s.dat" % (runNum, "time"), sep=" ", header=False, index=False)
+
+output_file = f"run{runNum}_time.dat"
+df_compare.to_csv(output_file, sep=" ", header=False, index=False)
+print("New time constants written to %s" % output_file)
